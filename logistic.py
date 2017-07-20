0116f5d9b5849d80ecff15bb1c02acc113a011a3
Alex Jordache <alexonej@gmail.com>
2017-07-20 09:20:51 -0400

    finish algo

--- logistic.py
+++ logistic.py
@@ -11,16 +11,54 @@ def sigmoid(z):
     return 1 / (1 + math.exp(-1 * z))
 
 '''
-Can be calculated using either vectorized or iterative versions
-vectorized = h(x) = 1 / 1 + e^ - (theta_transpose * X)
+The hypothesis can be calculated using either vectorized or iterative versions
+vectorized = h(x) = 1 / 1 + e ^ -1 * (theta_transpose * X)
 we can calculate theta_transpose * x by calculating the dot product
 (multiplying each column in the row vector theta_tranpose by the matching
-row in the column vector X and then sum up the products) 
+row in the column vector x and then sum up the products) 
 '''
-def hypothesis(theta, x):
+def hypothesis(theta,x):
     z = 0
     for i in range(len(theta)):
         z += theta[i] * x[i]
     return sigmoid(z)
 
+def cost_function(X,y,theta,m):
+    sumError = 0
+    for i in range(m):
+        error = 0
+        x_i = X[i]
+        y_i = y[i]
+        h_i = hypothesis(theta,x_i)
+        error = y_i * math.log(h_i) + (1 - y_i) * math.log(1 - h_i)
+        sumError += error
+    const = -1/m
+    cost = const * sumError
+    return cost
 
+def cfd(X,y,theta,j,m):
+    sumError = 0
+    for i in range(m):
+        x_i = X[i]
+        x_ij = x_i[j]
+        h_i = hypothesis(theta, x_i)
+        error = (h_i - y[i]) * x_ij
+        sumError += error
+    const = 1/m
+    cost = const * sumError
+    return cost
+
+def gradient_descent(X,y,theta,alpha,m):
+    opt_theta = []
+    for j in range(len(theta)):
+        cost = cfd(X,y,theta,j,m)
+        updated_theta = theta[j] - (alpha * cost)
+        opt_theta.append(updated_theta)
+    return opt_theta
+
+def Logistic_Regression(X, y, alpha, theta, iterations):
+    m = len(y)
+    for i in range(iterations):
+        opt_theta = gradient_descent(X,y,theta,alpha,m)
+        theta = opt_theta
+    return theta
